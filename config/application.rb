19280a44ba852863ac9fbd17566177dc690a061b
Yoshio Nose <ynose.249@gmail.com>
2014-09-16 13:53:45 +0000

    Bootstrap CDNに変更

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,5 @@ module Yq
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    config.server_static_assets = true
   end
 end
