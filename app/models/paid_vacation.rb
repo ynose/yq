59a0bac9a9594986c7dcc6f5ef6e9e7d9980d8eb
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2014-09-21 13:57:03 +0000

    環境再構築

--- app/models/paid_vacation.rb
+++ app/models/paid_vacation.rb
@@ -1,4 +1,7 @@
 class PaidVacation < ActiveRecord::Base
   belongs_to :user
 end
+
+#データ作成
 # paid_vacation = PaidVacation.new(:user_id=>131, :year=>2014, :hours=>160, :carryover_hours=>160)
+# paid_vacation.save
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
 end
 
+#データ作成
 #user = User.new(:id => 131, :name => "ynose", :email => "ynose.249@gmail.com")
+#user.save
