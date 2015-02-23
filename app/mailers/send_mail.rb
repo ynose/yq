12447109439ca1eac6330e3fcba9e393e376f3c6
ynose_fsd <nose@fsd-jp.com>
2015-02-23 18:10:03 +0900

    有給の日にちを件名と本文に埋め込み

--- app/mailers/send_mail.rb
+++ app/mailers/send_mail.rb
@@ -1,5 +1,5 @@
 class SendMail < ActionMailer::Base
-  default from: "from@example.com"
+  #default from: "from@example.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -7,9 +7,12 @@ class SendMail < ActionMailer::Base
   #   en.send_mail.sample_mail.subject
   #
   # 本文はapp/views/send_mail/sample_mail.text.erbにテンプレートを作成する
-  def sample_mail
-    @greeting = "Hi"
+  def sample_mail(vacation_id)
+    vacation = Vacation.find(vacation_id)
+    user = User.find(vacation.user_id)
 
-    mail to: "ynose.249@gmail.com", subject: "SendGrid Test"
+    @startdate = Date::new(vacation.start_datetime.year, vacation.start_datetime.month, vacation.start_datetime.day)
+
+    mail from: "#{user.email}", to: "ynose.249@gmail.com", subject: "#{user.name} - 有休 [#{@startdate.strftime("%m").to_s}月#{@startdate.strftime("%d").to_s}日]"
   end
 end
